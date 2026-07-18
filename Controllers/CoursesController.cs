092c5f7ab607e06c2b016ec9e1838b78cb14fa6e
salynajjar <salynajjar923@gmail.com>
2026-07-18 21:09:37 +0300

    Initial Course API project

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,133 @@
+﻿using CourseApi.Data;
+using CourseApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CourseApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+
+        public CoursesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        //POST
+        [HttpPost]
+        public async Task<ActionResult<Course>> CreateCourse(Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Courses.Add(course);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCourse),
+                new { id = course.Id },
+                course);
+        }
+
+        //GET by ID 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Course>> GetCourse(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return course;
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCourse(int id, Course updatedCourse)
+        {
+            if (id != updatedCourse.Id)
+            {
+                return BadRequest("Course ID mismatch.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            course.Title = updatedCourse.Title;
+            course.Instructor = updatedCourse.Instructor;
+            course.CreditHours = updatedCourse.CreditHours;
+            course.Price = updatedCourse.Price;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+     public async Task<IActionResult> DeleteCourse(int id )
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course==null)
+            {
+                return NotFound();
+            }
+
+            _context.Courses.Remove(course);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        //SEARCH
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Course>>> SearchCourses(string title)
+        {
+            var courses = await _context.Courses
+                .Where(c => c.Title.Contains(title))
+                .ToListAsync();
+
+            return courses;
+        }
+
+        //FILTERING
+        [HttpGet("filter")]
+        public async Task<ActionResult<IEnumerable<Course>>> FilterCourses(
+    decimal minPrice,
+    decimal maxPrice)
+        {
+            var courses = await _context.Courses
+                .Where(c => c.Price >= minPrice && c.Price <= maxPrice)
+                .ToListAsync();
+
+            return courses;
+        }
+
+
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+namespace CourseApi.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        
+
+        [Required (ErrorMessage="Course title is required.")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required (ErrorMessage ="Instructor name is required.")]
+        [StringLength(50)]
+        public string Instructor { get; set; }
+
+        [Range(1,6,ErrorMessage = "Credithours must be between 1 and 6 .")]
+
+        public int CreditHours { get; set; }
+
+        [Range (0,10000,ErrorMessage = "price must be greater than or equal to 0.")]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
